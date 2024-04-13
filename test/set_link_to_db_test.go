4384f51a02738fb46f9fcd08d5e01e0c6a6fe5b3
Serveresty <cs.go.12228@gmail.com>
2024-04-13 23:57:37 +0300

    tests for PostgreSQL methods

---
+++ test/set_link_to_db_test.go
@@ -0,0 +1,91 @@
+package test
+
+import (
+	"OZONTestCaseLinks/configs"
+	"OZONTestCaseLinks/database"
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+func TestSetReducedLinkToDbSuccess(t *testing.T) {
+	if err := godotenv.Load("../configs/.env"); err != nil {
+		log.Print("No .env file found")
+	}
+	dbCfg := configs.LoadConfig()
+	dbCfg.DbName = os.Getenv("DB_TEST_NAME")
+
+	err := database.DbInit(dbCfg)
+	if err != nil {
+		t.Fatalf("error db init: %s", err.Error())
+	}
+
+	err = database.CreateBaseTables()
+	if err != nil {
+		t.Fatalf("error creating base tables: %s", err.Error())
+	}
+
+	trs, err := database.DB.Begin(context.Background())
+	if err != nil {
+		t.Fatalf("error start transaction: %s", err.Error())
+	}
+
+	link, shortLink := "https://vk.com/feed", "ziT8WvYJRM"
+	err = database.SetReducedLinkToDB(shortLink, link)
+	if err != nil {
+		err = trs.Rollback(context.Background())
+		if err != nil {
+			t.Fatalf("error rollback transaction: %s", err.Error())
+		}
+		t.Fatalf("error insert into db: %s", err.Error())
+	}
+
+	err = trs.Rollback(context.Background())
+	if err != nil {
+		t.Fatalf("error rollback transaction: %s", err.Error())
+	}
+}
+
+func TestSetReducedLinkToDbErrorKeys(t *testing.T) {
+	if err := godotenv.Load("../configs/.env"); err != nil {
+		log.Print("No .env file found")
+	}
+	dbCfg := configs.LoadConfig()
+	dbCfg.DbName = os.Getenv("DB_TEST_NAME")
+
+	err := database.DbInit(dbCfg)
+	if err != nil {
+		t.Fatalf("error db init: %s", err.Error())
+	}
+
+	err = database.CreateBaseTables()
+	if err != nil {
+		t.Fatalf("error creating base tables: %s", err.Error())
+	}
+
+	trs, err := database.DB.Begin(context.Background())
+	if err != nil {
+		t.Fatalf("error start transaction: %s", err.Error())
+	}
+
+	link, shortLink := "https://vk.com/feed", "ziT8WvYJRM"
+	err = database.SetReducedLinkToDB(shortLink, link)
+	if err != nil {
+		err = trs.Rollback(context.Background())
+		if err != nil {
+			t.Fatalf("error rollback transaction: %s", err.Error())
+		}
+		t.Fatalf("error insert into db: %s", err.Error())
+	}
+
+	err = database.SetReducedLinkToDB(shortLink, link)
+	if err != nil {
+		err = trs.Rollback(context.Background())
+		if err != nil {
+			t.Fatalf("error rollback transaction: %s", err.Error())
+		}
+	}
+}
